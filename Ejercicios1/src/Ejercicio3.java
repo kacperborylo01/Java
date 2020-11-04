7cd9bf774659c1bb68288eb0bd19d5e59f3adcdc
Kacper <alum22kborylo01@cpifpbajoaragon.com>
2020-11-04 13:08:04 +0100

    Ejercicio2 Terminado

---
+++ Ejercicios1/src/Ejercicio3.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Ejercicio3 {
+
+	public static void main(String[] args) {
+		
+		int num1,num2;
+		int suma,multi,divi,resta;
+		
+		Scanner leer = new Scanner(System.in);
+		
+		System.out.println("Dime un numero");
+		num1 = leer .nextInt();
+		System.out.println("Dime otro numero");
+		num2 = leer .nextInt();
+		
+		suma = num1 + num2;
+		System.out.println("La suma de estos numeros es " + suma);
+		
+		resta = num1 - num2;
+		System.out.println("La resta de estos numeros es " + resta);
+		
+		multi = num1 * num2;
+		System.out.println("La multiplicación de estos numeros es " + multi);
+		
+		divi = num1 / num2;
+		System.out.println("La división de estos numeros es " + divi);
+		
+		
+
+	}
+
+}
