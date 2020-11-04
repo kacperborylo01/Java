8a59cd91da8cc8b5082917b7c2436ddda42d91d9
Kacper <alum22kborylo01@cpifpbajoaragon.com>
2020-11-04 12:16:55 +0100

    Iniciamos Java

---
+++ Ejercicios1/src/Ejercicio1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		
+		//Declaracion e inicialización variables
+		int num1=0,num2=0;
+		float real;
+		String nombre="";
+		boolean bandera=true;
+		
+		Scanner leer = new Scanner(System.in);
+		System.out.println("Dime tu nombre");
+		nombre = leer.nextLine();
+		System.out.println("Dime tu edad");
+		num1 = leer.nextInt();
+		
+		System.out.println(" Tu nombre es " + nombre + " y tienes " + num1 + " años ");
+		
+		
+		
+	}
+
+}
