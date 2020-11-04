32857fda80251ada0033ad3a8c1e043cbab92d43
Kacper <alum22kborylo01@cpifpbajoaragon.com>
2020-11-04 14:26:24 +0100

    Ejercicio5 Empezado

--- Ejercicios1/src/Ejercicio6.java
+++ Ejercicios1/src/Ejercicio6.java
@@ -1,8 +1,31 @@
+import java.util.Scanner;
 
 public class Ejercicio6 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int num1,num2,num3,multi,suma;
+		
+		Scanner leer = new Scanner(System.in);
+		
+		System.out.println("Dime el numero 1");
+		num1 = leer .nextInt();
+		
+		System.out.println("Dime el numero 2");
+		num2 = leer .nextInt();
+		
+		System.out.println("Dime el numero 3");
+		num3 = leer .nextInt();
+		
+		multi = num1*num2*num3;
+		suma = num1+num2+num3;
+		if(num1 < 0){
+			System.out.println("Como el primer numero que has introducido es negativo, realizaremos la multiplicacion de los 3 numeros " + multi);
+			
+		}else { 
+			System.out.println("Como el primer numero que has introducido NO es negativo, realizaremos la suma de los 3 numeros --- " + num1 + num2 + num3 + "=" + (suma));
+			
+		}
 
 	}
 
