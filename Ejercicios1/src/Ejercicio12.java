90afae851bfc15f6c2c19580832b238b75dafeda
Kacper <alum22kborylo01@cpifpbajoaragon.com>
2020-11-07 18:51:57 +0100

    Ejercicio 9 Terminado

---
+++ Ejercicios1/src/Ejercicio12.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Ejercicio12 {
+
+	public static void main(String[] args) {
+		Scanner leer = new Scanner(System.in);
+		
+		String bachiller,pruebaacc;
+		bachiller = "";
+		pruebaacc = "";
+				
+		
+		System.out.println("Tienes el titulo de Bachiller para acceder a este Ciclo Formativo?");
+		bachiller = leer .nextLine();
+		
+		if(bachiller.equalsIgnoreCase( "si" )){
+			
+			System.out.println("Puedes acceder");
+		}else{
+			System.out.println("Entonces has realizado una prueba de acceso?");
+			pruebaacc = leer .nextLine();
+		
+			if(pruebaacc.equalsIgnoreCase("si")){
+				System.out.println("Puedes acceder");
+			}else{
+				System.out.println("Lo sentimos,no puedes acceder, suerte para la próxima");
+				
+			}
+			
+		}
+		
+		
+		
+		
+		
+		
+
+	}
+	}
+
+
