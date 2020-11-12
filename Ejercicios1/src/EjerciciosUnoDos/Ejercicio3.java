51da81152b71d63760cd3963a8c724a8ebe4942d
Kacper <alum22kborylo01@cpifpbajoaragon.com>
2020-11-12 13:50:27 +0100

    Ejercicios 2-4 de los Ej-1.2 Terminados

---
+++ Ejercicios1/src/EjerciciosUnoDos/Ejercicio3.java
@@ -0,0 +1,21 @@
+package EjerciciosUnoDos;
+
+public class Ejercicio3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i = 0;
+		
+		while (i <= 100) {
+			if (i % 2 == 0) {
+				System.out.println(i);
+				
+			}
+			i++;
+		}
+		
+
+	}
+
+}
---
+++ Ejercicios1/src/EjerciciosUnoDos/Ejercicio4.java
@@ -0,0 +1,17 @@
+package EjerciciosUnoDos;
+
+public class Ejercicio4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 0,suma=0;
+		
+		while (i <= 100) {
+			suma = suma + i;
+			
+			i++;
+		}
+		System.out.println(suma);
+	}
+
+}
