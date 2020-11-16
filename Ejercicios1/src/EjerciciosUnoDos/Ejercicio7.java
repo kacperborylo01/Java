42eed4da26f6709b2c12dfe5da1df582cff792c5
Kacper <alum22kborylo01@cpifpbajoaragon.com>
2020-11-16 20:35:50 +0100

    Ejercicio7 de 1.2 Terminado

--- Ejercicios1/src/EjerciciosUnoDos/Ejercicio7.java
+++ Ejercicios1/src/EjerciciosUnoDos/Ejercicio7.java
@@ -3,7 +3,23 @@ package EjerciciosUnoDos;
 public class Ejercicio7 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int sumapares=0,sumanones=0,cont = 0;
+		
+		System.out.println("Numeros del 1 al 100");
+		
+		while  (cont <= 100){
+			if (cont % 2 == 0) {
+				sumapares = sumapares + cont;
+				
+			}else {
+				sumanones = sumanones + cont;
+			}
+			cont++;
+		}
+		
+		System.out.println("La suma de los numeros pares del 1 al 100 es: " + sumapares);
+		System.out.println("La suma de los numeros impares del 1 al 100 es: " + sumanones);
 
 	}
 
