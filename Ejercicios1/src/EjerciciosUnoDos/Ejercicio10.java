048d62e7870693b712335cd8d62ba7537a29452b
Kacper <alum22kborylo01@cpifpbajoaragon.com>
2020-11-17 19:46:42 +0100

    Ejercicios Empezados

---
+++ Ejercicios1/src/EjerciciosUnoDos/Ejercicio10.java
@@ -0,0 +1,27 @@
+package EjerciciosUnoDos;
+
+import java.util.Scanner;
+
+public class Ejercicio10 {
+
+	public static void main(String[] args) {
+		
+		Scanner leer = new Scanner(System.in);
+		
+		String frase,letra;
+		
+		int cont;
+		
+		System.out.println("Escribe una pabra o una frase");
+		frase = leer .nextLine();
+		
+		System.out.println("Ahora escribe la letra que quieres saber cuantas veces se muestra en la palabra o frase");
+		letra = leer .nextLine();
+		
+		for (int i  = 0;  length(frase); i ++) {
+			
+		}
+
+	}
+
+}
---
+++ Ejercicios1/src/EjerciciosUnoDos/Ejercicio9.java
@@ -0,0 +1,20 @@
+package EjerciciosUnoDos;
+
+public class Ejercicio9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int cont=0,num=1;
+
+		while (cont < 10) {
+			
+			while (num <= 10) {
+				System.out.println(num);
+				num = num + 1;
+				
+			}
+			cont = cont + 1;
+		}
+	}
+
+}
