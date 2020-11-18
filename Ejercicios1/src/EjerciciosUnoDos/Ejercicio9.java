befed1868d4c8a40b91abad06dcdaadd3d87714d
Kacper <alum22kborylo01@cpifpbajoaragon.com>
2020-11-18 14:10:35 +0100

    Ejericicio 9 Corregido

--- Ejercicios1/src/EjerciciosUnoDos/Ejercicio9.java
+++ Ejercicios1/src/EjerciciosUnoDos/Ejercicio9.java
@@ -4,16 +4,20 @@ public class Ejercicio9 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int cont=0,num=1;
 
-		while (cont < 10) {
+		for (int i = 1; i <= 10; i++) {
 			
-			while (num <= 10) {
-				System.out.println(num);
-				num = num + 1;
+			for (int j = 1; j <= 10; j++) {
+				System.out.println(j);
 				
-			}
-			cont = cont + 1;
+				
+			} 
+			System.out.println("----------------------");
+				
+				
+			
+			
+			
 		}
 	}
 
